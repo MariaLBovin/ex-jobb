af6b19ed422f3292847e823a300b7c56f4c02cb3
MariaLBovin <maria.bovin@gmail.com>
2024-01-09 13:49:33 +0100

    added desktop styling to search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,12 +3,17 @@ const Search = () => {
   return (
     <>
     <section className="search-container">
-        <h1 className="search-header">Vad ska jag läsa nu?</h1>
+        <div className="search-hero">
+          <img className="search-hero-img" src="/bookstack.png"></img>
+        </div>
         <div className="search-wrapper">
-            <article className="search-text">
+          <h1 className="search-header">Vad ska jag läsa nu?</h1>
+            <article className="search-wrapper-inner">
+              <p className="search-text">
             Vill du få tips om vad du ska läsa härnäst? 
             Skriv in titeln på din favoritbok eller den bok du senast läst så 
             genereras ett tips till dig baserat på ditt sökord.
+            </p>
             </article>
             <form className="search-form">
                 <input className="search-input" placeholder="Ange titel på din favoritbok"></input>
